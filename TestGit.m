1381d10bed4dc5585bf34e035c816a0fee29eac0
Paolo Roselli <paolo.roselli02@gmail.com>
2022-12-15 18:03:03 +0100

    add image resize

--- TestGit.m
+++ TestGit.m
@@ -1,6 +1,7 @@
 clear all; close all; clc
 
-imdata = imread('foto.png');
+imdata = imread('foto.jpg');
+imdata = imresize(imdata, [1080 1080]);
 figure(1);imshow(imdata); title('Original image')
 
 imdata = rgb2gray(imdata);
@@ -14,5 +15,5 @@ figure(3);imshow(S, []); title('Fuorier transform of image')
 Fsh = fftshift(F);
 figure(4);imshow(Fsh, []); title('Fuorier centered image')
 
-S2 = log(1+abs(Fsh));
+S2 = log(1+Fsh);
 figure(5);imshow(S2, []); title('log transformed image')
