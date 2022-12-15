a22829b693188da9696416fda64f92cd9c2fae2e
Paolo Roselli <paolo.roselli02@gmail.com>
2022-12-15 17:21:01 +0100

    add read images

--- TestGit.m
+++ TestGit.m
@@ -1,1 +1,18 @@
-test
+clear all; close all; clc
+
+imdata = imread('foto.png');
+figure(1);imshow(imdata); title('Original image')
+
+imdata = rgb2gray(imdata);
+figure(2);imshow(imdata); title('Gray image')
+
+F = fft2(imdata);
+
+S = abs(F);
+figure(3);imshow(S, []); title('Fuorier transform of image')
+
+Fsh = fftshift(F);
+figure(4);imshow(Fsh, []); title('Fuorier centered image')
+
+S2 = log(1+abs(Fsh));
+figure(5);imshow(S2, []); title('log transformed image')
