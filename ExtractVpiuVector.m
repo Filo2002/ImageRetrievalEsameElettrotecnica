ec2ff17eaa1ac6dda951ac191f5399bc96b2c155
Paolo Roselli <paolo.roselli02@gmail.com>
2022-12-16 10:45:16 +0100

    add separate function file

---
+++ ExtractVpiuVector.m
@@ -0,0 +1,18 @@
+function Vpiu = ExtractVpiuVector()
+    imagefiles = dir('*.jpg');     
+    nfiles = length(imagefiles);
+
+    Vpiu = zeros(nfiles, 62500);
+
+    for ii=1:nfiles
+        currentfilename = imagefiles(ii).name;
+        imdata = imread(currentfilename);
+        imdata = imresize(imdata, [250 250]);
+        imdata = rgb2gray(imdata);
+
+        VpiuKRow = clm(imdata); %matrix to row vector
+
+        Vpiu(ii,:) = VpiuKRow;
+    end
+    
+end
