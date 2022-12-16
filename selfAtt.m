5c6ac0ce5a4b58304abc438be0626e5f6bccd8a3
MarcoPetrosilli <mark99961@gmail.com>
2022-12-16 11:24:11 +0100

    from size to lenght

--- selfAtt.m
+++ selfAtt.m
@@ -2,7 +2,7 @@ function CalculatedSselfatt = selfAtt(Vrpiu,Vr,q,delta)
     orderedVVpiu = orderVector(Vrpiu,Vr,delta);
     orderedVpiuQ = orderVector(Vrpiu,q,delta);
     CalculatedSselfatt=0;
-    for i=1:size(Vr)
+    for i=1:length(Vr)
          CalculatedSselfatt=CalculatedSselfatt + abs(orderedVVpiu(i)-orderedVpiuQ(i));
     end
 end
