90407321711e9c894a80aaaf537be1618ebb3f3d
Paolo Roselli <paolo.roselli02@gmail.com>
2022-12-16 19:26:23 +0100

    get ordered vectors

--- main.m
+++ main.m
@@ -2,6 +2,22 @@ clear all; close all; clc
 
 loadImagesButton();
 
+imdata = imread('foto.jpg');
+imdata = imresize(imdata, [250 250]);
+imdata = rgb2gray(imdata);
+
+q = clm(imdata);
+
+nVpiu = lenght(Vpiu);
+orderedVectors = zeros(nVpiu, 62500);
+
+for ii=1:nVpiu
+    X = reshape(Vpiu(ii), 1, []); %matrix to row vector
+    orderedVectors(ii, :) = orderVector(X, q);
+end
+
+
+
 function loadImagesButton
 
     Vpiu = extractVpiuVector();
