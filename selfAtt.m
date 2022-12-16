2acf383a08f14ca4c11daf06c87357ffefd289ca
MarcoPetrosilli <mark99961@gmail.com>
2022-12-16 11:20:49 +0100

    add selfAtt function

---
+++ selfAtt.m
@@ -0,0 +1,8 @@
+function CalculatedSselfatt = selfAtt(Vrpiu,Vr,q,delta)
+    orderedVVpiu = orderVector(Vrpiu,Vr,delta);
+    orderedVpiuQ = orderVector(Vrpiu,q,delta);
+    CalculatedSselfatt=0;
+    for i=1:size(Vr)
+         CalculatedSselfatt=CalculatedSselfatt + abs(orderedVVpiu(i)-orderedVpiuQ(i));
+    end
+end
