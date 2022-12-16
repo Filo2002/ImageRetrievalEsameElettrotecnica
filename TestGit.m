f1ff0bc0836bbc5fbd58d051ec3f71321ffa5768
Filo2002 <talk.filippo@gmail.com>
2022-12-16 10:48:53 +0100

    use Clm function

--- TestGit.m
+++ TestGit.m
@@ -7,18 +7,9 @@ figure(1);imshow(imdata); title('Original image')
 imdata = rgb2gray(imdata);
 figure(2);imshow(imdata); title('Gray image')
 
-%trasformata di Fourier dell'immagine
-F = fft2(imdata); 
+immClm = clm(imdata);
 
-%trasformata di Fuorier centrata
-Fsh = fftshift(F);
-figure(4);imshow(Fsh, []); title('Fuorier centered image')
-
-%CLM(?) log della trasformata
-Vpiu = log(1+Fsh);
-figure(5);imshow(Vpiu, []); title('log transformed image')
-
-V = pinv(Vpiu);
+V = pinv(immClm);
 
 
 
