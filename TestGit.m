950a9d5067f46232b15b9973d26f5b8665fb9d8c
Paolo Roselli <paolo.roselli02@gmail.com>
2022-12-16 10:00:48 +0100

    remove unnecessary and change naming

--- TestGit.m
+++ TestGit.m
@@ -7,13 +7,13 @@ figure(1);imshow(imdata); title('Original image')
 imdata = rgb2gray(imdata);
 figure(2);imshow(imdata); title('Gray image')
 
-F = fft2(imdata);
+%trasformata di Fourier dell'immagine
+F = fft2(imdata); 
 
-S = abs(F);
-figure(3);imshow(S, []); title('Fuorier transform of image')
-
+%trasformata di Fuorier centrata
 Fsh = fftshift(F);
 figure(4);imshow(Fsh, []); title('Fuorier centered image')
 
-S2 = log(1+Fsh);
-figure(5);imshow(S2, []); title('log transformed image')
+%CLM(?) log della trasformata
+Vpiu = log(1+Fsh);
+figure(5);imshow(Vpiu, []); title('log transformed image')
