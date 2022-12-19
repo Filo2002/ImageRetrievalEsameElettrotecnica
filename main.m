7d7f6ad424e7cbadfc80e83427ce783690b88b51
Paolo Roselli <paolo.roselli02@gmail.com>
2022-12-19 16:43:04 +0100

    user-feedback works with default setted image

--- main.m
+++ main.m
@@ -5,62 +5,88 @@ loadImagesButton();
 Vpiu = extractVpiuVector();
 V = pinv(Vpiu);
 
+choseIndex = mainFunction(Vpiu, V);
 imageSize = StoreData.GetImageSize();
-
 directory = 'Images/foto_icon/';
-imagefiles = dir(strcat(directory, '*.jpg'));     
-totImage = length(imagefiles);
+imagefiles = dir(strcat(directory, '*.jpg'));  
 
-simBest = zeros(totImage, 2);
+%feedback di prova impostando prefissata l'immagine 3
+currentfilename = imagefiles(choseIndex).name;
+imdata = imread(strcat(directory, currentfilename));
+imdata = imresize(imdata, [imageSize imageSize]);
+imdata = im2gray(imdata);
+q = clm(imdata);
+vsi = pinv(q);
 
-for index=1:totImage
+vnew = feedback(V(:,3),vsi);
 
-    currentfilename = imagefiles(index).name;
-    imdata = imread(strcat(directory, currentfilename));
-    imdata = imresize(imdata, [imageSize imageSize]);
-    imdata = im2gray(imdata);
-    q = clm(imdata);
-    
-    nVpiu = size(Vpiu);
-    nVpiu = nVpiu(1);
-    orderedVectors = zeros(nVpiu, imageSize.^2);
-    
-    for ii=1:nVpiu
-        X = Vpiu(ii, :); %matrix to row vector
-        orderedVectors(ii, :) = orderVector(X, q, 50);   %delta 5 sembra al limite per attendibilità
-    end
-    
-    similarityVec = zeros(nVpiu,1);
-    selfAttVec = zeros(nVpiu,1);
+V(:,3) = vnew;
+Vpiu = pinv(V);
 
-    
-    for ii=1:nVpiu  
-    
-        similarityVec(ii) = sumVector(orderedVectors(ii, :));
-        selfAttVec(ii) = selfAtt(Vpiu(ii, :), V(:, ii), q, 50);
-    end
-    
-    simNorm = normalization(similarityVec(3), similarityVec); %1 corrisponde alla 1 foto per la ricerca, in questo caso il cerchio.
-    selfAttNorm = normalization(selfAttVec(3), selfAttVec);
-    simBest(index, 1) = abs(simNorm);
-    simBest(index, 2) = abs(selfAttNorm);
-end
+mainFunction(Vpiu, V);
 
-simBest
-sortedVect = sortVector(simBest, StoreData.GetMaximumImages());
-
-for jj=1:StoreData.GetMaximumImages()
-
-    currentfilename = imagefiles(sortedVect(jj)).name;
-    imdata = imread(strcat(directory, currentfilename));
-    figure(jj);imshow(imdata); title(jj)
-
-end
 
 function loadImagesButton
 
     Vpiu = extractVpiuVector();
     V = pinv(Vpiu);
 
+end
+
+function choseIndex = mainFunction(Vpiu, V)
+    close all;
+    imageSize = StoreData.GetImageSize();
+    
+    directory = 'Images/foto_icon/';
+    imagefiles = dir(strcat(directory, '*.jpg'));     
+    totImage = length(imagefiles);
+    
+    simBest = zeros(totImage, 2);
+    
+    for index=1:totImage
+    
+        currentfilename = imagefiles(index).name;
+        imdata = imread(strcat(directory, currentfilename));
+        imdata = imresize(imdata, [imageSize imageSize]);
+        imdata = im2gray(imdata);
+        q = clm(imdata);
+        
+        nVpiu = size(Vpiu);
+        nVpiu = nVpiu(1);
+        orderedVectors = zeros(nVpiu, imageSize.^2);
+        
+        for ii=1:nVpiu
+            X = Vpiu(ii, :); %matrix to row vector
+            orderedVectors(ii, :) = orderVector(X, q, 50);   %delta 5 sembra al limite per attendibilità
+        end
+        
+        similarityVec = zeros(nVpiu,1);
+        selfAttVec = zeros(nVpiu,1);
+    
+        
+        for ii=1:nVpiu  
+        
+            similarityVec(ii) = sumVector(orderedVectors(ii, :));
+            selfAttVec(ii) = selfAtt(Vpiu(ii, :), V(:, ii), q, 50);
+        end
+        
+        simNorm = normalization(similarityVec(3), similarityVec); %1 corrisponde alla 1 foto per la ricerca, in questo caso il cerchio.
+        selfAttNorm = normalization(selfAttVec(3), selfAttVec);
+        simBest(index, 1) = abs(simNorm);
+        simBest(index, 2) = abs(selfAttNorm);
+    end
+
+    simBest
+    sortedVect = sortVector(simBest, StoreData.GetMaximumImages());
+    
+    for jj=1:StoreData.GetMaximumImages()
+    
+        currentfilename = imagefiles(sortedVect(jj)).name;
+        imdata = imread(strcat(directory, currentfilename));
+        figure(jj);imshow(imdata); title(jj)
+    
+    end
+
+    choseIndex = sortedVect(5);
 end
 
